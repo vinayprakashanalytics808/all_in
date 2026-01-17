0a9c7c90ea53abcea25495d374731169cd0b11c8
vinayprakash808 <vinayprakash808@gmail.com>
2026-01-17 23:55:00 +0530

    changes

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
-
+from flask import Flask, jsonify, render_template
+from models.get_stocks import stock_data
+import requests
+import pandas as pd
 app = Flask(__name__)
-
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World Vin!'
+def index():
+    return render_template('index.html')
 
+@app.route('/update_table', methods=['POST'])
+def update_table():
+    my_com = stock_data(["Tata Steel", "Reliance", "Mukka Proteins"], 'currentPrice')
+    my_com.get_data_from_api()
+    return jsonify({'message': 'Data updated in DB'})
+
+##"Reliance", "Mukka Proteins"
+@app.route('/stock_details', methods=['GET'])
+def next_page():
+    my_com = stock_data(["Tata Steel", "Reliance", "Mukka Proteins"], 'currentPrice')
+    return my_com.get_data_from_api()
 
 if __name__ == '__main__':
     app.run()
